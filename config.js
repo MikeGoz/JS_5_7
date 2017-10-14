d7db2af7bebcd2bf81968970042576d1285b8224
MikeGoz <gozdz@onet.pl>
2017-10-14 18:44:24 +0200

    Task 5.7

---
+++ config.js
@@ -0,0 +1,10 @@
+// Task 5.7
+
+module.exports = {
+	'GOOGLE_CLIENT_ID': '30866411334-r5672c6m0sljroh5g51nl2o2kt8dgpn9.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'ArPKf4Zfz3yiIaMdx5z4LMGU',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
+
+
